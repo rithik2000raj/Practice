8f2d5b628bbf5dac132f35d5cd664848d5573a20
rithik2000raj <rithik2000raj@gmail.com>
2024-08-17 15:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ submit = st.button("Submit")
 if submit:
     try:
         # Load Model
-        model = pickle.load(open('model_rf.pkl', 'rb'))
+        model = pickle.load(open('model1.pkl', 'rb'))
         
         # Process Input
         input_df_split = input_df.split(',')
